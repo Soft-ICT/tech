3678309e3c55bccef9f22d08db39954fdb058062
Jewel Rana <136537973+JewelBP@users.noreply.github.com>
2026-08-15 22:40:44 +0600

    Update firebas.js

--- webapp/js/firebas.js
+++ webapp/js/firebas.js
@@ -1,49 +1,34 @@
 import { initializeApp } from
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-import {
-    getDatabase
-} from
+import { getAuth } from
+    "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getDatabase } from
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
 
 const firebaseConfig = {
-
-    apiKey: "YOUR_API_KEY",
-
-    authDomain:
-        "qrt-team.firebaseapp.com",
-
-    databaseURL:
-        "https://qrt-team-default-rtdb.firebaseio.com",
-
-    projectId:
-        "qrt-team",
-
-    storageBucket:
-        "qrt-team.firebasestorage.app",
-
-    messagingSenderId:
-        "855875068920",
-
-    appId:
-        "1:855875068920:web:df1a2218ee083000b8ec0d",
-
-    measurementId:
-        "G-BKNYLRYSGS"
-
+    apiKey: "AIzaSyCSirbiyWf7WQn9UqoZghAfph2U5jwJbPI",
+    authDomain: "qrt-team.firebaseapp.com",
+    databaseURL: "https://qrt-team-default-rtdb.firebaseio.com",
+    projectId: "qrt-team",
+    storageBucket: "qrt-team.firebasestorage.app",
+    messagingSenderId: "855875068920",
+    appId: "1:855875068920:web:df1a2218ee083000b8ec0d",
+    measurementId: "G-BKNYLRYSGS"
 };
 
 
-const app =
-    initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
 
-const db =
-    getDatabase(app);
+const db = getDatabase(app);
 
 
 export {
     app,
+    auth,
     db
 };
