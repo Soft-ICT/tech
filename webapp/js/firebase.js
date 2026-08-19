4b73a91b0cd62af95de093a902facfa484dacceb
Jewel Rana <136537973+JewelBP@users.noreply.github.com>
2026-08-19 11:35:43 +0600

    Refactor Firebase setup and initialization
    Updated Firebase initialization to use the new method and removed direct imports.

--- webapp/js/firebase.js
+++ webapp/js/firebase.js
@@ -1,20 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCSirbiyWf7WQn9UqoZghAfph2U5jwJbPI",
-    authDomain: "qrt-team.firebaseapp.com",
-    databaseURL: "https://qrt-team-default-rtdb.firebaseio.com",
-    projectId: "qrt-team",
-    storageBucket: "qrt-team.firebasestorage.app",
-    messagingSenderId: "855875068920",
-    appId: "1:855875068920:web:df1a2218ee083000b8ec0d",
-    measurementId: "G-BKNYLRYSGS"
+    apiKey: "AIzaSyCSirbiyWf7WQn9UqoZghAfph2U5jwJbPI",[span_2](start_span)[span_2](end_span)
+    authDomain: "qrt-team.firebaseapp.com",[span_3](start_span)[span_3](end_span)
+    databaseURL: "https://qrt-team-default-rtdb.firebaseio.com",[span_4](start_span)[span_4](end_span)
+    projectId: "qrt-team",[span_5](start_span)[span_5](end_span)
+    storageBucket: "qrt-team.firebasestorage.app",[span_6](start_span)[span_6](end_span)
+    messagingSenderId: "855875068920",[span_7](start_span)[span_7](end_span)
+    appId: "1:855875068920:web:df1a2218ee083000b8ec0d",[span_8](start_span)[span_8](end_span)
+    measurementId: "G-BKNYLRYSGS[span_9](start_span)"[span_9](end_span)
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
-export { app, auth, db };
+export const auth = firebase.auth();
+export const db = firebase.database();
