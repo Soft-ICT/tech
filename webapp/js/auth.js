0cb3bb29d00822e3dd40bfb3cf0ad4cdbc60e0e8
Jewel Rana <136537973+JewelBP@users.noreply.github.com>
2026-08-19 11:35:07 +0600

    Refactor auth.js to use updated Firebase Auth methods

--- webapp/js/auth.js
+++ webapp/js/auth.js
@@ -1,40 +1,32 @@
-import { 
-    getAuth, 
-    signInWithEmailAndPassword, 
-    onAuthStateChanged,
-    signOut
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { app } from "./firebase.js";
-
-const auth = getAuth(app);
+import { auth } from "./firebase.js";
 
 // Admin Login Function
 export async function loginAdmin(email, password) {
     try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        return { success: true, user: userCredential.user };
+        const userCredential = await auth.signInWithEmailAndPassword(email, password);[span_10](start_span)[span_10](end_span)
+        return { success: true, user: userCredential.user };[span_11](start_span)[span_11](end_span)
     } catch (error) {
-        return { success: false, error: error.message };
+        return { success: false, error: error.message };[span_12](start_span)[span_12](end_span)
     }
 }
 
 // Admin Logout Function
 export async function logoutAdmin() {
     try {
-        await signOut(auth);
-        return { success: true };
+        await auth.signOut();[span_13](start_span)[span_13](end_span)
+        return { success: true };[span_14](start_span)[span_14](end_span)
     } catch (error) {
-        return { success: false, error: error.message };
+        return { success: false, error: error.message };[span_15](start_span)[span_15](end_span)
     }
 }
 
 // Auth State Watcher
 export function watchAuth(callback) {
-    onAuthStateChanged(auth, (user) => {
+    auth.onAuthStateChanged((user) => {[span_16](start_span)[span_16](end_span)
         if (user) {
-            callback(user, "admin");
+            callback(user, "admin");[span_17](start_span)[span_17](end_span)
         } else {
-            callback(null, null);
+            callback(null, null);[span_18](start_span)[span_18](end_span)
         }
     });
 }
