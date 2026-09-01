5db572729450a4cb4ac9b5f11668909038c930a9
Jewel Rana <136537973+JewelBP@users.noreply.github.com>
2026-09-01 08:33:39 +0600

    Update Firebase configuration in service worker

--- webapp/firebase-messaging-sw.js
+++ webapp/firebase-messaging-sw.js
@@ -2,12 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/12.1.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCSirbiyWf7WQn9UqoZghAfph2U5jwJbPI",
+    authDomain: "qrt-team.firebaseapp.com",
+    databaseURL: "https://qrt-team-default-rtdb.firebaseio.com",
+    projectId: "qrt-team",
+    storageBucket: "qrt-team.firebasestorage.app",
+    messagingSenderId: "855875068920",
+    appId: "1:855875068920:web:df1a2218ee083000b8ec0d",
+    measurementId: "G-BKNYLRYSGS"
 });
 
 const messaging = firebase.messaging();
