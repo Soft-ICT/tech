96eeaab46020ba2a02f3abe42860c68820a74688
Jewel Rana <136537973+JewelBP@users.noreply.github.com>
2026-09-02 07:43:28 +0600

    Fix formatting of firebase configuration object

--- webapp/js/firebase.js
+++ webapp/js/firebase.js
@@ -4,14 +4,14 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-
 import { getMessaging } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSirbiyWf7Qn9UqoZghAfph2U5jwJbPI",
-    authDomain: "qrt-team.firebaseapp.com",
-    databaseURL: "https://qrt-team-default-rtdb.firebaseio.com",
-    projectId: "qrt-team",
-    storageBucket: "qrt-team.firebasestorage.app",
-    messagingSenderId: "855875068920",
-    appId: "1:855875068920:web:df1a2218ee083000b8ec0d",
-    measurementId: "G-BKNYLRYSGS"
+apiKey: "AIzaSyCSirbiyWf7WQn9UqoZghAfph2U5jwJbPI",
+authDomain: "qrt-team.firebaseapp.com",
+databaseURL: "https://qrt-team-default-rtdb.firebaseio.com",
+projectId: "qrt-team",
+storageBucket: "qrt-team.firebasestorage.app",
+messagingSenderId: "855875068920",
+appId: "1:855875068920:web:df1a2218ee083000b8ec0d",
+measurementId: "G-BKNYLRYSGS"
 };
 
 const app = initializeApp(firebaseConfig);
